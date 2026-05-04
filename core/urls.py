bc93fa72dba087e8db308331f65a2adce884aca6
Sujal <sujallimbu31@gmail.com>
2026-05-04 20:22:45 +0545

    added active color to the links

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('' , home , name = 'home')
+    path('' , home , name = 'home'),
+    path('about/' , about , name = 'about'),
+    path('contact/' , contact , name = 'contact'),
+    path('services/' , services , name = 'services'),
+    path('projects/' , projects , name = 'projects')
 ]
