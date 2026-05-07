89eaeedd068c5acdf5a9a9c50b15f9074e13168f
Sujal <sujallimbu31@gmail.com>
2026-05-07 11:04:22 +0545

    Fixed Responsiveness

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
